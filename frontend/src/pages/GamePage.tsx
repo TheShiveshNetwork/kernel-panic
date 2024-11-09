45309f36e600cbbbc1c74a82755a87dccc2ae4b6
unknown <shiveshtiwari0@gmail.com>
2024-11-09 22:40:38 +0530

    added terminal ui

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+
 function GamePage() {
+    const [terminalLineData, setTerminalLineData] = useState([
+        <TerminalOutput>Welcome to the React Terminal UI Demo!</TerminalOutput>
+    ]);
     return (
-        <>
-            <h1>Page to render the terminal game</h1>
-        </>
+        <div className="container">
+            <Terminal
+                name='Kernel Panic'
+                colorMode={ColorMode.Dark}
+                onInput={terminalInput => console.log(`New terminal input received: '${terminalInput}'`)}
+            >
+                {terminalLineData}
+            </Terminal>
+        </div>
     );
 }
 
