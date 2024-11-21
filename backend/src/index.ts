1e6c378b0db367700f24b94fbe56ef0ddbf3a10a
TheShiveshNetwork <shiveshtiwari0@gmail.com>
2024-11-22 02:02:55 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,14 @@ const io = new Server(server, {
   },
 });
 
+// Manual CORS Configuration
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
