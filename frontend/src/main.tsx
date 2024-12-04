20c1e68659efbfdf57d0e86fda31f88c4e5e4a71
unknown <shiveshtiwari0@gmail.com>
2024-12-04 22:28:32 +0530

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import { Router } from '@/router';
-import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <Router/>
-    </StrictMode>
+  <StrictMode>
+    <Router/>
+  </StrictMode>,
 )
